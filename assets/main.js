bfc0d699baade003d233a9aeb20cf5c45eb6e6d4
IllustratorCrow <illustratorcrow@gmail.com>
2025-01-19 20:03:45 +0100

    Update main.js

--- assets/main.js
+++ assets/main.js
@@ -1,12 +1,12 @@
 let slideIndex = 1;
 showSlides(slideIndex);
 
-// Funciones de control de los botones
+// Next/previous controls
 function plusSlides(n) {
   showSlides(slideIndex += n);
 }
 
-// Función para controlar los puntos de navegación
+// Thumbnail image controls
 function currentSlide(n) {
   showSlides(slideIndex = n);
 }
@@ -15,24 +15,23 @@ function showSlides(n) {
   let i;
   let slides = document.getElementsByClassName("mySlides");
   let dots = document.getElementsByClassName("dot");
-  
-  // Aseguramos que el índice esté dentro de los límites
+
   if (n > slides.length) {slideIndex = 1}
   if (n < 1) {slideIndex = slides.length}
-  
-  // Ocultar todas las imágenes
+
+  // Hide all slides
   for (i = 0; i < slides.length; i++) {
     slides[i].style.display = "none";
   }
 
-  // Desactivar los puntos
+  // Remove the "active" class from all dots
   for (i = 0; i < dots.length; i++) {
     dots[i].className = dots[i].className.replace(" active", "");
   }
 
-  // Mostrar la imagen actual
+  // Show the current slide
   slides[slideIndex-1].style.display = "block";
 
-  // Activar el punto correspondiente
+  // Add the "active" class to the current dot
   dots[slideIndex-1].className += " active";
 }
