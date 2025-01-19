cc1d22791e2054eb45264688688509b7e35c8019
IllustratorCrow <illustratorcrow@gmail.com>
2025-01-19 20:32:11 +0000

    Deploying to gh-pages from @ IllustratorCrow/Portfolio@a3ef903b14b5a095ae5968f4c1b8293e76a85fcf 🚀

--- assets/main.js
+++ assets/main.js
@@ -1,12 +1,10 @@
 let slideIndex = 1;
 showSlides(slideIndex);
 
-// Next/previous controls
 function plusSlides(n) {
   showSlides(slideIndex += n);
 }
 
-// Thumbnail image controls
 function currentSlide(n) {
   showSlides(slideIndex = n);
 }
@@ -19,19 +17,14 @@ function showSlides(n) {
   if (n > slides.length) {slideIndex = 1}
   if (n < 1) {slideIndex = slides.length}
   
-  // Hide all slides
   for (i = 0; i < slides.length; i++) {
     slides[i].style.display = "none";
   }
-
-  // Remove "active" class from all dots
+  
   for (i = 0; i < dots.length; i++) {
     dots[i].className = dots[i].className.replace(" active", "");
   }
   
-  // Display the current slide
   slides[slideIndex-1].style.display = "block";
-  
-  // Add "active" class to the current dot
   dots[slideIndex-1].className += " active";
 }
