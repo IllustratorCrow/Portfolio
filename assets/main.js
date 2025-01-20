cc5c7cb4b2f52c8c48dd700f757e36ab14ea22d0
IllustratorCrow <illustratorcrow@gmail.com>
2025-01-20 13:49:37 +0000

    Deploying to gh-pages from @ IllustratorCrow/Portfolio@9a572947746fa1661e14ab2b0bbb46345529c609 🚀

--- assets/main.js
+++ assets/main.js
@@ -1,37 +1,30 @@
-let slideIndex = 0;
-showSlides();
+let slideIndex = 1;
+    showSlides(slideIndex);
 
-function showSlides() {
-  const slides = document.getElementsByClassName("mySlides");
-  const dots = document.getElementsByClassName("dot");
+    function plusSlides(n) {
+      showSlides(slideIndex += n);
+    }
 
-  // Ocultar todas las diapositivas
-  for (let i = 0; i < slides.length; i++) {
-    slides[i].style.display = "none";
-  }
+    function currentSlide(n) {
+      showSlides(slideIndex = n);
+    }
 
-  slideIndex++;
+    function showSlides(n) {
+      let i;
+      let slides = document.getElementsByClassName("mySlides");
+      let dots = document.getElementsByClassName("dot");
 
-  // Volver al primer slide si se supera el límite
-  if (slideIndex > slides.length) {
-    slideIndex = 1;
-  }
+      if (n > slides.length) {slideIndex = 1}
+      if (n < 1) {slideIndex = slides.length}
 
-  // Desactivar todas las "dots"
-  for (let i = 0; i < dots.length; i++) {
-    dots[i].className = dots[i].className.replace(" active", "");
-  }
+      for (i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";
+      }
 
-  // Mostrar el slide actual y activar la "dot" correspondiente
-  slides[slideIndex - 1].style.display = "block";
-  dots[slideIndex - 1].className += " active";
+      for (i = 0; i < dots.length; i++) {
+        dots[i].className = dots[i].className.replace(" active", "");
+      }
 
-  // Cambiar diapositiva cada 5 segundos
-  setTimeout(showSlides, 5000);
-}
-
-// Controles manuales
-function currentSlide(n) {
-  slideIndex = n - 1; // Ajustar índice para la función
-  showSlides();
-}
+      slides[slideIndex - 1].style.display = "block";
+      dots[slideIndex - 1].className += " active";
+    }
