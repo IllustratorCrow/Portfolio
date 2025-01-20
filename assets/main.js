15793ed1c430956525fff6ccb744b600df46306a
IllustratorCrow <illustratorcrow@gmail.com>
2025-01-20 13:58:17 +0000

    Deploying to gh-pages from @ IllustratorCrow/Portfolio@99887cc9185dff7f0194e5822116e9b6dec271d7 🚀

--- assets/main.js
+++ assets/main.js
@@ -1,30 +1,32 @@
 let slideIndex = 1;
-    showSlides(slideIndex);
+showSlides(slideIndex);
 
-    function plusSlides(n) {
-      showSlides(slideIndex += n);
-    }
+// Control manual del slider
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
 
-    function currentSlide(n) {
-      showSlides(slideIndex = n);
-    }
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
 
-    function showSlides(n) {
-      let i;
-      let slides = document.getElementsByClassName("mySlides");
-      let dots = document.getElementsByClassName("dot");
+// Mostrar las diapositivas
+function showSlides(n) {
+  let i;
+  let slides = document.getElementsByClassName("mySlides");
+  let dots = document.getElementsByClassName("dot");
 
-      if (n > slides.length) {slideIndex = 1}
-      if (n < 1) {slideIndex = slides.length}
+  if (n > slides.length) { slideIndex = 1; }
+  if (n < 1) { slideIndex = slides.length; }
 
-      for (i = 0; i < slides.length; i++) {
-        slides[i].style.display = "none";
-      }
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
 
-      for (i = 0; i < dots.length; i++) {
-        dots[i].className = dots[i].className.replace(" active", "");
-      }
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" active", "");
+  }
 
-      slides[slideIndex - 1].style.display = "block";
-      dots[slideIndex - 1].className += " active";
-    }
+  slides[slideIndex - 1].style.display = "block";
+  dots[slideIndex - 1].className += " active";
+}
