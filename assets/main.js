a9dbd3c3a520f94c945540e840fb50376ac5cfee
IllustratorCrow <illustratorcrow@gmail.com>
2025-01-20 14:03:15 +0100

    Update main.js

--- assets/main.js
+++ assets/main.js
@@ -12,7 +12,7 @@ function currentSlide(n) {
 function showSlides(n) {
   const slides = document.querySelectorAll(".mySlides");
   const dots = document.querySelectorAll(".dot");
-
+  
   if (n > slides.length) slideIndex = 1;
   if (n < 1) slideIndex = slides.length;
 
