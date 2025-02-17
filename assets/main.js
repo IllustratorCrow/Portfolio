8760bd054b2553aa454695de1cc664f1162bf3ed
IllustratorCrow <illustratorcrow@gmail.com>
2025-02-17 13:53:02 +0100

    Update main.js

--- assets/main.js
+++ assets/main.js
@@ -1,22 +1,42 @@
 let slideIndex = 0;
 showSlides();
 
+function showSlides() {
+    const slides = document.getElementsByClassName("mySlides");
+    const dots = document.getElementsByClassName("dot");
+
+    for (let i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";
+    }
+
+    slideIndex++;
+
+    if (slideIndex > slides.length) {
+        slideIndex = 1;
+    }
+
+    for (let i = 0; i < dots.length; i++) {
+        dots[i].className = dots[i].className.replace(" active", "");
+    }
+
+    slides[slideIndex - 1].style.display = "block";
+    dots[slideIndex - 1].className += " active";
+
+    setTimeout(showSlides, 5000);
+}
+
+// Controles manuales
 function plusSlides(n) {
-  showSlides(slideIndex += n);
+    slideIndex += n;
+    if (slideIndex < 1) {
+        slideIndex = document.getElementsByClassName("mySlides").length;
+    } else if (slideIndex > document.getElementsByClassName("mySlides").length) {
+        slideIndex = 1;
+    }
+    showSlides();
 }
 
 function currentSlide(n) {
-  showSlides(slideIndex = n);
-}
-
-function showSlides() {
-  let slides = document.getElementsByClassName("mySlides");
-  if (slideIndex >= slides.length) {slideIndex = 0}
-  if (slideIndex < 0) {slideIndex = slides.length - 1}
-  
-  for (let i = 0; i < slides.length; i++) {
-    slides[i].style.display = "none";  // Oculta todas las imágenes
-  }
-  
-  slides[slideIndex].style.display = "block";  // Muestra la imagen actual
+    slideIndex = n;
+    showSlides();
 }
